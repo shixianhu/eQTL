ecc225cce6e1e0b79d89eee2daeb6b42195f41e4
rbarbieri86 <32519681+rbarbieri86@users.noreply.github.com>
2019-07-26 14:17:18 +0200

    Delete get_variant_from_doubleids.sh
    Obsolete codes

--- Tools/get_variant_from_doubleids.sh
+++
@@ -1,6 +0,0 @@
-#!\bin\bash
-while IFS='' read line; do
-	filename_part=$(echo ${line} | cut -d '_' -f 1)
-	filename="${filename_part}_doubleids_list.txt"
-	awk '{print $1}' $line > "$filename"
-done < filelist_doubleids.txt
