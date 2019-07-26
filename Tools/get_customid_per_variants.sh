956c403d2507227c10f93a3ba552a8a6bf533c91
rbarbieri86 <32519681+rbarbieri86@users.noreply.github.com>
2019-07-26 14:17:42 +0200

    Delete get_customid_per_variants.sh
    Obsolete code

--- Tools/get_customid_per_variants.sh
+++
@@ -1,6 +0,0 @@
-#!\bin\bash
-while IFS='' read line; do
-	filename_part=$(echo ${line} | cut -d '_' -f 1)
-	filename="${filename_part}_doubleids.txt"
-	grep -wf $line ../165test_passonly.noXnoY.hwe.nomiss.maf5percent_doubleids_cor.txt | awk '{print $1"\t"$2}' > "$filename"
-done < filelist_eqtls.txt
