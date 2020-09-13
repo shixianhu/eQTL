3b8dcb61165ecf77d0f61ebb9331d1773a5a431e
shixianhu <32686413+shixianhu@users.noreply.github.com>
2020-09-13 17:18:18 +0200

    Update Re-assess_cis-eQTL.R

--- lme4qtl/Re-assess_cis-eQTL.R
+++ lme4qtl/Re-assess_cis-eQTL.R
@@ -1,5 +1,5 @@
 # we used GEMMA for an interaction model (gene expression = SNP + inflammation + SNP * inflammation) to calculate inflammation-dependent cis-eQTL
-# however, we only got summary statistics of interaction term
+# however, we only got summary statistics of interaction term from GEMMA by default
 # to get full summary statistics of term 'SNP' and 'inflammation', we used lme4qtl R package to re-calculate 190 significant (FDR interaction <0.05) from GEMMA
 # in addition, we also compared the results derived from GEMMA and lme4qtl, and compared three models to adjust for random effect
 
